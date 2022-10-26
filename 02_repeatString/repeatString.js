f030bb37be3481a14b7cddd499bcfa0c9d6483ce
Frank Alimimian <alimimianfrank@gmail.com>
2022-10-26 09:07:20 +0100

    Edit repeatString.js

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
+const repeatString = function(words, num){
+  let string = "";
+  if(num < 0) return "ERROR";
+  for(i = 0; i < num; i++){
+    string +=words;
+  }
+  return string;
+}
 
-};
 
 // Do not edit below this line
 module.exports = repeatString;
