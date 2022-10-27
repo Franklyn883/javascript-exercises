df5f244954847855bf70a10b9ac3f99beb62624b
Frank Alimimian <alimimianfrank@gmail.com>
2022-10-27 07:06:23 +0100

    Edit removeFromArray.js

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(...arr) {
+  const array = arr[0];
+  const newArray = [];
+  array.forEach((item) => {
+    if(!arr.includes(item)){
+      newArray.push(item)
+    }
+    
+  });
+return newArray;
 };
 
 // Do not edit below this line
